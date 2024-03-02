5bac1533b58b5f8fef6bac17fc9953da7873c285
mammoth <mammmoth@list.ru>
2024-03-03 02:23:31 +0300

    prep3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,17 @@ mod generator;
 mod ui;
 mod gen_engine;
 
+use eframe::egui;
+
 fn main() {
-    println!("Hello, world!");
+    ui.heading("My egui Application");
+    ui.horizontal(|ui| {
+        ui.label("Your name: ");
+        ui.text_edit_singleline(&mut name);
+    });
+    ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
+    if ui.button("Increment").clicked() {
+        age += 1;
+    }
+    ui.label(format!("Hello '{name}', age {age}"));
 }
