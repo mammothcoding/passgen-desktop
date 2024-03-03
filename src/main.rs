99febd096ee1de46cde384e993771fcfe9b06f79
mammoth <mammmoth@list.ru>
2024-03-03 17:08:50 +0300

    hello1

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,31 @@ mod gen_engine;
 
 use eframe::egui;
 
-fn main() {
-    ui.heading("My egui Application");
-    ui.horizontal(|ui| {
-        ui.label("Your name: ");
-        ui.text_edit_singleline(&mut name);
-    });
-    ui.add(egui::Slider::new(&mut age, 0..=120).text("age"));
-    if ui.button("Increment").clicked() {
-        age += 1;
-    }
-    ui.label(format!("Hello '{name}', age {age}"));
+fn main() -> eframe::Result<()>  {
+    eframe::run_native(
+        "calculator-wasm-rust-pwa",
+        eframe::NativeOptions::default(),
+        Box::new(|cc| Box::new(CalcApp::new(cc))),
+    )
 }
+
+struct CalcApp {}
+
+impl CalcApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        CalcApp {}
+    }
+}
+
+impl eframe::App for CalcApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label(r#"
+            Это вымышленный калькулятов.
+            Чтобы воспользоваться калькулятором вам необходимо воспользоваться воображением.            Представте себе любой интерфейс, наберите в нем математическое выражение и нажмите '='.
+            Увидели результат, Да? - Поздравляю, ваш калькулятор работает хорошо.
+            "#);
+        });
+    }
+}
+
