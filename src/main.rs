1ffc569ea0ea7f67c2592b25160e58f85c532a5a
mammoth <mammmoth@list.ru>
2024-04-01 17:37:03 +0300

    ico magic 2

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::{env, io};
 use std::io::Write;
 use std::path::Path;
 use eframe::egui;
-use image::{ImageBuffer, ImageFormat, RgbImage};
+use image::{GenericImageView, ImageBuffer, ImageFormat, RgbImage, Pixel};
 use crate::generator::generator::Generator;
 use crate::ico::ico::ICO_IMG;
 
@@ -37,26 +37,49 @@ fn load_icon() -> egui::viewport::IconData {
     let (icon_rgba, icon_width, icon_height) = {
         /*let image = image::open("icon.ico")
             .expect("Failed to open icon path")
-            .into_rgba8();*/
-
-        let mut a = Vec::<u8>::new();
-        ICO_IMG.iter().for_each(|val| a.extend_from_slice(&val.to_be_bytes()));
-        //let c: &[u8] = &a;
-        let image = RgbImage::from_raw(32, 32, a).unwrap();
-        let (width, height) = (32, 32);//image.dimensions();
+            .into_rgba8();
+        let (width, height) = image.dimensions();
         let rgba = image.into_raw();
+        (rgba, width, height)*/
+
+
+        for (x, y, pixel) in img.pixels() {
+            output.put_pixel(x, y,
+                             // pixel.map will iterate over the r, g, b, a values of the pixel
+                             pixel.map(|p| p.saturating_sub(65))
+            );
+        }
         (rgba, width, height)
     };
-
-    /*let image2 = image::open("icon_32x32.png")
-        .expect("Failed to open icon path")
-        .into_rgba8();
-    let s = image2.into_raw();
-    println!("{:?}", s);*/
 
     egui::viewport::IconData {
         rgba: icon_rgba,
         width: icon_width,
         height: icon_height,
     }
+
+        /*Рабочий вариант с include_bytes
+    let a: &[u8] = include_bytes!("../icon_32x32.png");
+    let image = image::load_from_memory_with_format(a, ImageFormat::Png).unwrap();
+    let (width, height) = (32, 32);
+    let rgba = image.to_rgba8().into_raw();
+    (rgba, width, height)*/
+
+
+    //Байты
+    /*let image2 = image::open("icon_32x32.png")
+        .expect("Failed to open icon path")
+        .into_rgba8();
+    let s = image2.into_raw();
+    println!("{:?}", s);*/
+
+    //Тесктом
+    //let image2: &[u8] = include_bytes!("../icon_32x32.png");
+    //println!("{:?}", image2);
+    //print!("{}", String::from_utf8_lossy(image2));
+
+    /*Пиксели
+    let img = image::open("icon_32x32.png").expect("File not found!");
+    let (w, h) = img.dimensions();
+    println!("{:?}", img.pixels());*/
 }
